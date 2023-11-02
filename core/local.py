4b65755f92a5301bb5fbff74409fc72c29a6a6c7
Benjamin Rosario <benjaminfl119@gmail.com>
2023-11-02 15:40:54 -0400

    eliminado el archvio local.py

--- core/local.py
+++
@@ -1,8 +0,0 @@
-DB_ENGINE = "django.db.backends.mysql"
-DB_NAME = "recycling"
-DB_USER = "root"
-DB_PASSWORD = ""
-DB_HOST = "localhost"
-DB_PORT = "3306"
-
-ALLOWED_ORIGINS = ["http://localhost:3006"]
