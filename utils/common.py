76589c3d1f32c4b1f6f8d9957aa84c8b2247b9fe
Benjamin Rosario <benjaminfl119@gmail.com>
2023-11-02 13:54:09 -0400

    Fix anotation

--- utils/common.py
+++ utils/common.py
@@ -47,9 +47,7 @@ class BaseModel(models.Model):
             raise APIException(e) from e
 
     @classmethod
-    def create_many(
-        cls: models.Model, request, data_list: list[dict]
-    ) -> list[models.Model]:
+    def create_many(cls: models.Model, request, data_list: list[dict]) -> list:
         try:
             created_objects = []
             objects_to_create = []
