11cef15cefa8b94ccb73b66afd39964bc3383e50
Benjamin Rosario <benjaminfl119@gmail.com>
2023-11-02 16:39:32 -0400

    updating local.py for heroku

---
+++ core/local.py
@@ -0,0 +1,8 @@
+DB_ENGINE = "django.db.backends.mysql"
+DB_NAME = "yvihj12cesarl5me"
+DB_USER = "up5wd12y2b2csits"
+DB_PASSWORD = "pj9w7x6u4356cdby"
+DB_HOST = "ebh2y8tqym512wqs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com"
+DB_PORT = "3306"
+
+ALLOWED_ORIGINS = ["https://clean-cycle-client-fjf7holsa-fenixfl1.vercel.app"]
