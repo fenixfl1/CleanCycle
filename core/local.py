4d489e9098ddc198277e4c181f8f4acfef971d33
Benjamin Rosario <benjaminfl119@gmail.com>
2023-11-03 21:28:29 -0400

    deleting local.py

--- core/local.py
+++
@@ -1,8 +0,0 @@
-DB_ENGINE = "django.db.backends.mysql"
-DB_NAME = "xww9ayrteyh6phm2"
-DB_USER = "t2o1zxisvjozytwo"
-DB_PASSWORD = "b2avsgyz2bgut8d6"
-DB_HOST = "ckshdphy86qnz0bj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com"
-DB_PORT = "3306"
-
-ALLOWED_ORIGINS = ["https://clean-cycle-client-fjf7holsa-fenixfl1.vercel.app"]
