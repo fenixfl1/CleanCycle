d034ae84958dc16d24f1944b675a4bfbfe9b60cb
Benjamin Rosario <benjaminfl119@gmail.com>
2023-11-03 21:21:12 -0400

    updated database info

--- core/local.py
+++ core/local.py
@@ -1,8 +1,8 @@
 DB_ENGINE = "django.db.backends.mysql"
-DB_NAME = "yvihj12cesarl5me"
-DB_USER = "up5wd12y2b2csits"
-DB_PASSWORD = "pj9w7x6u4356cdby"
-DB_HOST = "ebh2y8tqym512wqs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com"
+DB_NAME = "xww9ayrteyh6phm2"
+DB_USER = "t2o1zxisvjozytwo"
+DB_PASSWORD = "b2avsgyz2bgut8d6"
+DB_HOST = "ckshdphy86qnz0bj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com"
 DB_PORT = "3306"
 
 ALLOWED_ORIGINS = ["https://clean-cycle-client-fjf7holsa-fenixfl1.vercel.app"]
